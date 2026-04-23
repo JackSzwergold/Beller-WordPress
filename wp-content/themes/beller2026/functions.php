089d35c2d187db568cffdeb0bf6ccff95817ca33
Jack Szwergold <szwergoj@newschool.edu>
2026-04-23 15:32:02 -0400

    More work.

--- wp-content/themes/beller2026/functions.php
+++ wp-content/themes/beller2026/functions.php
@@ -465,7 +465,7 @@ class Dropdown_Walker_Nav_Menu extends Walker_Nav_Menu {
 		if ($depth === 0 && $args->walker->has_children) {
 			$toggle_link = !empty($item->url) ? $item->url : '#';
 			$output .=
-				  '<a href="' . esc_url($toggle_link) . '" class="dropdown-toggle nav-link bg-beige px-5 ' . $a_class_names . '" data-submenu="' . $item->ID . '" role="button" data-bs-toggle="dropdown" aria-expanded="false">'
+				  '<a href="' . esc_url($toggle_link) . '" class="dropdown-toggle nav-link px-5 ' . $this->bg_menu . ' ' . $a_class_names . '" data-submenu="' . $item->ID . '" role="button" data-bs-toggle="dropdown" aria-expanded="false">'
 				. $item->title
 				. '</a>'
 				;
@@ -473,7 +473,7 @@ class Dropdown_Walker_Nav_Menu extends Walker_Nav_Menu {
 		else if ($depth > 0 && $args->walker->has_children) { 
 			$toggle_link = !empty($item->url) ? $item->url : '#';
 			$output .=
-			  '<a href="' . esc_url($toggle_link) . '" class="dropdown-toggle dropdown-item bg-beige px-5 ' . $a_class_names . '" data-submenu="' . $item->ID . '">'
+			  '<a href="' . esc_url($toggle_link) . '" class="dropdown-toggle dropdown-item px-5 ' . $this->bg_menu . ' ' . $a_class_names . '" data-submenu="' . $item->ID . '">'
 			. $item->title
 			. '</a>'
 			;
@@ -481,14 +481,14 @@ class Dropdown_Walker_Nav_Menu extends Walker_Nav_Menu {
 		else {
 			if ($depth > 0) {
 				$output .= 
-					  '<a href="' . $item->url . '" class="dropdown-item bg-beige ' . $a_class_names . '" data-submenu="' . $item->ID . '">'
+					  '<a href="' . $item->url . '" class="dropdown-item ' . $this->bg_menu . ' ' . $a_class_names . '" data-submenu="' . $item->ID . '">'
 					. $item->title
 					. '</a>'
 					;				
 			} // if
 			else {
 				$output .= 
-					  '<a href="' . $item->url . '" class="dropdown-item bg-beige mx-5 ' . $a_class_names . '" data-submenu="' . $item->ID . '">'
+					  '<a href="' . $item->url . '" class="dropdown-item mx-5 ' . $this->bg_menu . ' ' . $a_class_names . '" data-submenu="' . $item->ID . '">'
 					. $item->title
 					. '</a>'
 					;				
