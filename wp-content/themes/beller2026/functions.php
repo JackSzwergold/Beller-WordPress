7af8d351ce5655701631fcd5c3eb155be87af27d
Jack Szwergold <szwergoj@newschool.edu>
2026-05-05 14:15:44 -0400

    More work.

--- wp-content/themes/beller2026/functions.php
+++ wp-content/themes/beller2026/functions.php
@@ -237,10 +237,10 @@ function single_post () {
 
     /**************************************************************************/
     // The article image stuff.
-	$article_image = wp_get_attachment_image_url(get_post_thumbnail_id($the_ID), 'medium');
+	$article_image = wp_get_attachment_image_url(get_post_thumbnail_id($the_ID), 'large');
     if (isset($article_image) && !empty($article_image)) {
       $article_image =
-          '<div class="col col-12 col-md-2 m-0 p-0 mb-2 ms-md-3 float-end">'
+          '<div class="col col-12 col-md-3 m-0 p-0 mb-2 ms-md-3 float-end">'
         . '<a href="' . $permalink . '" title="' . $title . '" class="text-decoration-none text-dark">'
         . '<img src="' . $article_image . '" alt="' . $title . '" class="img-fluid">'
         . '</a>'
