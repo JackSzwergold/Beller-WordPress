1679d50b6e79cfef935702edd00392a63fe0e0ee
Jack Szwergold <szwergoj@newschool.edu>
2026-05-07 15:33:06 -0400

    More work.

--- wp-content/themes/beller2026/header.php
+++ wp-content/themes/beller2026/header.php
@@ -245,7 +245,7 @@
 	</nav>
 
 	<header class="sticky-top p-0 m-0">
-		<nav class="navbar navbar-expand bg-transparent col col-12 p-0 m-0">
+		<nav class="navbar navbar-expand col col-12 p-0 m-0">
 			<?php
 				primary_menu();
 			?>
